108aa7e1802c5ffe47db3ecf1a781aa09efc2bea
Tomas Morton <tomas.morton@broadsign.com>
2020-12-01 19:15:08 -0500

    Update formatting

--- DayOne/Program.fs
+++ DayOne/Program.fs
@@ -1,22 +1,24 @@
 ﻿open System.IO
 
-let readLines (filePath:string) = seq {
-    use sr = new StreamReader (filePath)
-    while not sr.EndOfStream do
-        yield sr.ReadLine ()
-}
+let readLines (filePath : string) =
+    seq {
+        use sr = new StreamReader(filePath)
+        while not sr.EndOfStream do
+            yield sr.ReadLine()
+    }
 
 [<EntryPoint>]
 let main argv =
     let goal = 2020
-    
+
     let input =
         readLines "input.txt"
         |> Seq.map int
         |> Seq.toList
-    
+
     Seq.allPairs input input
     |> Seq.find (fun (x, y) -> x + y = goal)
     |> fun (x, y) -> x * y
-    |> printfn "%d" 
+    |> printfn "%d"
+
     0 // return an integer exit code
