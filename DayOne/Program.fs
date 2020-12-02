45ff3963e8d7269334c15d649b9a2476f21a683d
Tomas Morton <tomas.morton@broadsign.com>
2020-12-01 19:13:41 -0500

    Add day one problem and solution

---
+++ DayOne/Program.fs
@@ -0,0 +1,22 @@
+﻿open System.IO
+
+let readLines (filePath:string) = seq {
+    use sr = new StreamReader (filePath)
+    while not sr.EndOfStream do
+        yield sr.ReadLine ()
+}
+
+[<EntryPoint>]
+let main argv =
+    let goal = 2020
+    
+    let input =
+        readLines "input.txt"
+        |> Seq.map int
+        |> Seq.toList
+    
+    Seq.allPairs input input
+    |> Seq.find (fun (x, y) -> x + y = goal)
+    |> fun (x, y) -> x * y
+    |> printfn "%d" 
+    0 // return an integer exit code
